ba3b3b4ae874c13ed0fd5d8f0fc89b6d84530640
Kokat111 <kacperorczykowksi@gmail.com>
2021-05-31 12:00:58 +0200

    V1

--- skrypt.py
+++ skrypt.py
@@ -1,3 +1,6 @@
+import numpy as np
+import sys
+
 def Bombel(array):
 
 	n=len(array)
@@ -7,8 +10,14 @@ def Bombel(array):
 		for j in range(n-i-1):
 			if array[j]>array[j+1]:
 				array[j],array[j+1] = array[j+1],array[j]
-array=[1,31,4,2334,5,2,44,32,42,48,46,5,6,567,45,7,8,6,9,632]
 
+
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+array=np.random.randint(b,c,size=a)
 Bombel(array)
 
 for i in range(len(array)):
